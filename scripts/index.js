c7b0537780f5204b12d1c2f85b1ae977fb7b9088
kingerious <78227395+MDxWARRIORxOP@users.noreply.github.com>
2022-10-24 16:49:58 +0530

    Add files via upload

---
+++ scripts/index.js
@@ -0,0 +1,22 @@
+import { world } from "mojang-minecraft";
+
+// whenever someone sends a msg, just repeat it.
+world.events.beforeChat.subscribe((event) => {
+  world.say(event.message);
+});
+
+// whenever someone breaks a block, just say the name of the block.
+world.events.blockBreak.subscribe((event) => {
+  world.say(event.block.type.id);
+});
+
+// whenever theres an explosion, just say the source of it.
+world.events.beforeExplosion.subscribe((event) => {
+  world.say(event.source.id);
+  world.say("BOOM!");
+});
+
+// whenever someone places a block, just say the name of the block.
+world.events.blockPlace.subscribe((event) => {
+  world.say(event.block.type.id);
+});
